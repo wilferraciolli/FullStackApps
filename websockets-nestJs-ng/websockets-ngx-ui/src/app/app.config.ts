ce2f59cb3ffa6764df5761a5796d4cf5a2a9a643
Wiliam Ferraciolli <wiliam334@hotmail.com>
2025-04-21 21:04:37 +0300

    ui - Fixed socket config

--- websockets-nestJs-ng/websockets-ngx-ui/src/app/app.config.ts
+++ websockets-nestJs-ng/websockets-ngx-ui/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {socketConfig} from './sockets/services/socket.service';
 import {provideHttpClient} from '@angular/common/http';
+import {SocketIoModule} from 'ngx-socket-io';
 
 
 export const appConfig: ApplicationConfig = {
@@ -11,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(),
-    {provide: 'SOCKET_CONFIG', useValue: socketConfig}
-    // importProvidersFrom(SocketIoModule.forRoot(socketConfig))
+    importProvidersFrom(SocketIoModule.forRoot(socketConfig))
   ]
 };
--- websockets-nestJs-ng/websockets-ngx-ui/src/main.ts
+++ websockets-nestJs-ng/websockets-ngx-ui/src/main.ts
@@ -1,23 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
-import {importProvidersFrom} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
 
-// Get the socket config
-const socketConfig = appConfig.providers.find(
-  provider => provider && typeof provider === 'object' && 'provide' in provider && provider.provide === 'SOCKET_CONFIG'
-);
-
-const socketIoConfig = socketConfig && 'useValue' in socketConfig ? socketConfig.useValue as SocketIoConfig : {
-  url: 'http://localhost:3001',
-  options: {}
-};
-
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    ...appConfig.providers,
-    importProvidersFrom(SocketIoModule.forRoot(socketIoConfig))
-  ]})
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
